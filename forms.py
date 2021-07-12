f8b4508bb2c9b1a4a575178a1466c82f543885de
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-07-12 20:55:22 +0000

    finished user input

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+# from flask import Flask, render_template, url_for, flash, redirect
+# from forms import RegistrationForm
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
