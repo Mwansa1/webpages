4356c8e587d92cb56c562ab53144326c6f45c5c1
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-07-13 17:41:42 +0000

    sensor data

---
+++ audio.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+from os import path
+
+FILE_NAME = "static/poem.wav"
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), FILE_NAME)
+
+r = sr.Recognizer()
+
+time = 0 # position in track
+dur = 10 # clip length to process each iteration
+
+def printWAV(FILE_NAME, pos, clip):
+    # use the audio file as the audio source
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), 'static/'+FILE_NAME)
+    r = sr.Recognizer()
+    text = ""
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source, duration=clip, offset=pos)
+        # recognize speech using Google Speech Recognition
+        try:
+            text += r.recognize_google(audio) + "\n"
+        except sr.UnknownValueError:
+            text += "Could not understand audio\n"
+        except sr.RequestError as e:
+            text += "Could not request results; {0}".format(e)+ "\n"
+    return text
---
+++ main.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+from audio import printWAV
+import time, random, threading
+from turbo_flask import Turbo
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '17e81666b51cd4989ff8a76af64ba52a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+interval=10
+FILE_NAME = "poem.wav"
+turbo = Turbo(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+  
+
+
+def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    
+@app.route("/second_page")
+def second_page():
+    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+      user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+      db.session.add(user)
+      db.session.commit()
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)  
+
+@app.route("/captions")
+def captions():
+    TITLE = "Test_Audio"
+#     FILE_NAME = "examples_english.wav"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+            
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
